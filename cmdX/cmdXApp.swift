785b8cbc346bdb81dccb03a1f78e417c1f82c96c
Yonn <yonn.apps@outlook.com>
2025-10-09 18:59:44 +0200

    Initial Commit

---
+++ cmdX/cmdXApp.swift
@@ -0,0 +1,17 @@
+//
+//  cmdXApp.swift
+//  cmdX
+//
+//  Created by Yann Pump on 09.10.25.
+//
+
+import SwiftUI
+
+@main
+struct cmdXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
